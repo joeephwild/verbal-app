4987e6f39deb2eabc22a36e4b9faddf3335951dc
Joseph Omotade <josephomotade0@gmail.com>
2023-07-26 19:04:49 +0100

    commit

---
+++ src/navigation/AppNavigation.tsx
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const AppNavigation = () => {
+  return (
+    <View>
+      <Text>AppNavigation</Text>
+    </View>
+  );
+};
+
+export default AppNavigation;
