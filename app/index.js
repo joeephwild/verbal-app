17728fd5e4df1682a1e399f872a97b1baba5806a
ambiti0n <fabulousfresh010@gmail.com>
2023-07-30 00:19:10 +0100

    feat: supabase auth

--- app/index.js
+++ app/index.js
@@ -1,13 +1,36 @@
 import { StatusBar } from "expo-status-bar";
 import { Text, View } from "react-native";
+import React, { useEffect, Component } from "react";
+import { supabase } from "./lib/supabase";
+import Auth from "./components/Auth";
 
 export default function Page() {
+  const [session, setSession] = React.useState(0);
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    supabase.auth.onAuthStateChange(async (event, session) => {
+      console.log("sessions", event, session, session && session.user);
+      if (session && session.user) {
+        console.log("fetching");
+        const { data, error } = await supabase
+          .from("user_profiles")
+          .select("*")
+          .eq("id", session.user.id);
+        console.log(data, error);
+      }
+    });
+  }, []);
   return (
-    <View className="flex-1 items-center justify-center bg-black">
-      <Text className="text-white">
-       joseph
-      </Text>
-      <StatusBar style="auto" />
+    <View>
+      {session && session.user ? (
+        <Account key={session.user.id} session={session} />
+      ) : (
+        <Auth />
+      )}
     </View>
   );
 }
