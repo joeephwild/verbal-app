6c10e8cd0a1b13426716f0128e10dd6742cdec4c
joseph <josephomotade0@gmail.com>
2024-06-01 04:32:07 +0100

    commit

--- verbal-contract/hardhat.config.js
+++ verbal-contract/hardhat.config.js
@@ -7,8 +7,12 @@ const PRIVATE_KEY = process.env.PRIVATE_KEY
 module.exports = {
   solidity: "0.8.19",
   networks: {
-    alfajores: {
-      url: "https://alfajores-forno.celo-testnet.org/",
+    zkSyncEraSepoliaTestnet: {
+      url: "https://mainnet.era.zksync.io",
+      accounts: [`1ef03a878f42f3ef7b4b132f72d3666ed71bb3f70defe8ee5c2ffd6874b84b78`],
+    },
+    zkSyncEraSepoliaMainnet: {
+      url: "https://sepolia.era.zksync.dev",
       accounts: [`1ef03a878f42f3ef7b4b132f72d3666ed71bb3f70defe8ee5c2ffd6874b84b78`],
     },
   }
